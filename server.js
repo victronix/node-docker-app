324dcb63ec9817d477031068e74408aaa31bf192
dell <dell@ubuntu.ubuntu-domain>
2020-08-17 13:12:02 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const HOST = "0.0.0.0";
+
+const PORT = 8080;
+
+const app = express();
+
+app.get("/",(req, res) => {
+    res.send("Hello Victronix")
+});
+
+
+app.listen(PORT, HOST, (res) => {
+    console.log("Server running on PORT: " + PORT);
+})
+
